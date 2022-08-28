2da24afd237b12ceb2f10fc101f5da8ad87a100a
Alexander Zaytsev <yup.disconnected.yup@gmail.com>
2022-08-28 15:06:34 +0300

    WIP: trying to display content with curses

--- sw/StreamLineCLI/main.py
+++ sw/StreamLineCLI/main.py
@@ -1,42 +1,103 @@
 import serial.tools.list_ports
 from datetime import datetime
+import curses
+from curses import wrapper
+import time
 
 ports = serial.tools.list_ports.comports()
 serialInst = serial.Serial()
 
 portList = []
 
-for onePort in ports:
-    portList.append(str(onePort))
-    print(str(onePort))
 
-val = input("Enter COM port number to connect: ")
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Press any key to continue...")
+    stdscr.getch()
+    stdscr.clear()
+    stdscr.addstr("Select [Number] of COM port to connect:\n\r")
 
-for x in range(0,len(portList)):
-    if portList[x].startswith("COM" + str(val)):
-        portVar = "COM" + str(val)
-        print(portList[x])
+    for onePort in ports:
+        portList.append(str(onePort))
+        stdscr.addstr(str(onePort))
 
-# Create list
-dataDict = {}
-tempList = []
+    val = stdscr.getkey()
+    stdscr.clear()
+    stdscr.addstr(f"val: {val}")
+    stdscr.getch()
 
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
+    for x in range(0, len(portList)):
+        if portList[x].startswith("COM" + str(val)):
+            portVar = "COM" + val
+            stdscr.addstr(str(portList[x]))
 
-serialInst.baudrate = 115200
-serialInst.port = portVar
-serialInst.open()
+    # Create list
+    dataDict = {}
+    tempList = []
 
-while True:
-    if serialInst.in_waiting:
-        packet = serialInst.readline()
-        if packet[1] == 0:
-            now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
-            tempDict = {current_time: tempList.copy()}
-            dataDict.update(tempDict)
-            tempList.clear()
-            continue
-        tempList.append(packet.decode('utf').rstrip('\n'))
-        print(packet.decode('utf').rstrip('\n'))
+    # Serial Configuration
+    serialInst.baudrate = 115200
+    serialInst.port = portVar
+    serialInst.open()
+
+    stdscr.clear()
+    stdscr.addstr("Open port\n\r")
+    stdscr.getch()
+    stdscr.clear()
+    while True:
+        if serialInst.in_waiting:
+            packet = serialInst.readline()
+            if packet[0] == 0:
+                now = datetime.now()
+                current_time = now.strftime("%H:%M:%S")
+                tempDict = {current_time: tempList.copy()}
+                dataDict.update(tempDict)
+                stdscr.clear()
+                for x in range(len(tempList)):
+                    stdscr.addstr(str(tempList[x]))
+                    stdscr.refresh()
+                tempList.clear()
+                continue
+            # tempList.append(packet.decode('utf').rstrip('\n'))
+            my_string = str(packet.decode('utf').rstrip('\n'))
+            my_string = my_string.rstrip('\0')
+            tempList.append(my_string)
+
+
+wrapper(main)
+
+# for onePort in ports:
+#     portList.append(str(onePort))
+#     print(str(onePort))
+#
+# val = input("Enter COM port number to connect: ")
+#
+# for x in range(0,len(portList)):
+#     if portList[x].startswith("COM" + str(val)):
+#         portVar = "COM" + str(val)
+#         print(portList[x])
+#
+# # Create list
+# dataDict = {}
+# tempList = []
+#
+# now = datetime.now()
+# current_time = now.strftime("%H:%M:%S")
+#
+# serialInst.baudrate = 115200
+# serialInst.port = portVar
+# serialInst.open()
+#
+# while True:
+#     if serialInst.in_waiting:
+#         packet = serialInst.readline()
+#         if packet[1] == 0:
+#             now = datetime.now()
+#             current_time = now.strftime("%H:%M:%S")
+#             print(current_time)
+#             tempDict = {current_time: tempList.copy()}
+#             dataDict.update(tempDict)
+#             tempList.clear()
+#             continue
+#         tempList.append(packet.decode('utf').rstrip('\n'))
+#         print(packet.decode('utf').rstrip('\n'))
