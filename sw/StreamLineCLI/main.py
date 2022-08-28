b556501ce9df728163f050ed80d9ae9d05617f57
Alexander Zaytsev <yup.disconnected.yup@gmail.com>
2022-08-28 18:06:33 +0300

    Create two windows for data and hints

--- sw/StreamLineCLI/main.py
+++ sw/StreamLineCLI/main.py
@@ -10,7 +10,26 @@ serialInst = serial.Serial()
 
 portList = []
 
+DEFAULT_BAUDRATE = 115200
+
+HINTS_WINDOW_HEIGHT = 6
+HINTS_WINDOW_WIDTH  = 80
+HINTS_WINDOW_X_POS  = 0
+HINTS_WINDOW_Y_POS  = 0
+
+DATA_WINDOW_HEIGHT = 15
+DATA_WINDOW_WIDTH  = 50
+DATA_WINDOW_X_POS  = 0
+DATA_WINDOW_Y_POS  = HINTS_WINDOW_HEIGHT
+
 def main(stdscr):
+
+    # Display Stream data content
+    data_window = curses.newwin(DATA_WINDOW_HEIGHT, DATA_WINDOW_WIDTH, DATA_WINDOW_Y_POS, DATA_WINDOW_X_POS)
+
+    # Display hints, shortkeys and other info
+    hints_window = curses.newwin(HINTS_WINDOW_HEIGHT, HINTS_WINDOW_WIDTH, HINTS_WINDOW_Y_POS, HINTS_WINDOW_X_POS)
+
     stdscr.clear()
     stdscr.addstr("Press any key to continue...")
     stdscr.getch()
@@ -29,34 +48,42 @@ def main(stdscr):
             stdscr.clear()
             stdscr.addstr(str(portList[x]))
             stdscr.refresh()
-
-    # Create list
-    dataDict = {}
-    tempList = []
+    # Create list to store current frame
+    frame = []
 
     # Serial Configuration
-    serialInst.baudrate = 115200
+    serialInst.baudrate = DEFAULT_BAUDRATE
     serialInst.port = portVar
     serialInst.open()
 
     stdscr.clear()
     stdscr.refresh()
+
+    # Display hints
+    hints_window.clear()
+    hints_window.addstr('Connected to: {} '.format(portVar))
+    hints_window.addstr('with {} baudrate\n'.format(DEFAULT_BAUDRATE))
+    hints_window.addstr('-----------------------------------------------------------------------\n')
+    hints_window.addstr('Press [NUMBER] to ENABLE/DISABLE output or [KEY] to toggle state\n')
+    hints_window.addstr('[1]ADC0 [2]ADC1 [3]LED0 [4]LED1 [5]LED2 [6]LED3 [7]BTN0 [8]HLD0 [9]TMP0\n')
+    hints_window.addstr('[B]LED0 [R]LED1 [O]LED2 [G]LED3\n')
+    hints_window.addstr('-----------------------------------------------------------------------')
+    hints_window.refresh()
+
     while True:
         if serialInst.in_waiting:
             packet = serialInst.readline()
             if packet[0] == 0:
                 now = datetime.now()
                 current_time = now.strftime("%H:%M:%S")
-                tempDict = {current_time: tempList.copy()}
-                dataDict.update(tempDict)
-                stdscr.clear()
-                for x in range(len(tempList)):
-                    stdscr.addstr(x, 0, str(tempList[x]))
-                    stdscr.refresh()
-                tempList.clear()
+                data_window.clear()
+                for x in range(len(frame)):
+                    data_window.addstr(x, 0, str(frame[x]))
+                    data_window.refresh()
+                frame.clear()
                 continue
             my_string = str(packet.decode('utf').rstrip('\n'))
             my_string = my_string.replace('\0','')
-            tempList.append(my_string)
+            frame.append(my_string)
 
 wrapper(main)
